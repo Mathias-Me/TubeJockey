f1bb085528a9f6b7822e60f45042b30fd2d1c98d
Mathias-Me <mathiasmentler@web.de>
2023-02-04 22:16:49 +0100

    Added first youtube-dl option on webserver.

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+import youtube_dl
+ydl_opts = {}
+
 app = FastAPI()
 
 app.add_middleware(
@@ -14,5 +17,12 @@ app.add_middleware(
 @app.get("/")
 async def main():
     return {"message": "Hello World!"}
+
+@app.get("/download")
+async def download(url):
+    url = url.replace('"','')
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+    return {"success" : "True"}
 
 # Run by uvicorn main:app --reload
